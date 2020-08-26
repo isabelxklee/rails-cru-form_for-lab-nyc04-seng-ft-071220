3d72ae8269d756cb2fda3dd5d8db9ba61e463aca
isabel k. lee <hello@isabelklee.co>
2020-08-26 14:49:11 -0400

    finished artist and genre deliverables

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,7 +6,7 @@ class ArtistsController < ApplicationController
   def create
     @artist = Artist.new(artist_params)
     @artist.save
-    redirect_to artists_path
+    redirect_to artist_path(@artist)
   end
 
   def index
@@ -15,6 +15,16 @@ class ArtistsController < ApplicationController
 
   def show
     @artist = Artist.find(params[:id])
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params)
+    redirect_to artist_path(@artist)
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
-Artist.create(name: "Adele", bio: "She's a singer.")
+Artist.destroy_all
+Genre.destroy_all
+Song.destroy_all
+
+adele = Artist.create(name: "Adele", bio: "She's a singer.")
+meg = Artist.create(name: "Megan Thee Stallion", bio: "She's a rapper.")
+
+pop = Genre.create(name: "Pop")
+rap = Genre.create(name: "Rap")
+
+Song.create(name: "Hello", artist_id: adele.id, genre_id: pop.id)
+Song.create(name: "Savage", artist_id: meg.id, genre_id: rap.id)
 
 puts "Seeded!"
