1b923fa78d977146f271a051a6904e4164d7f828
isabel k. lee <hello@isabelklee.co>
2020-08-26 09:50:57 -0400

    started artists deliverables

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,24 @@
+class ArtistsController < ApplicationController
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    @artist.save
+    redirect_to artists_path
+  end
+
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  private
+  def artist_params
+    params.require(:artist).permit(:name, :bio)
+  end
+end
